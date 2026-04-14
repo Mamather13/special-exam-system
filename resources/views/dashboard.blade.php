efb7c3da9c1f84a181d57a05c57b6bb1157d4f1e
rjamesdelossantos5-ui <rjamesdelossantos5@gmail.com>
2026-04-14 22:58:22 +0800

    h

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
         </div>
-        <flux:button variant="primary">Apply </flux:button>
+        <flux:button variant="primary">Apply h</flux:button>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
