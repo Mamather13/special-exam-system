71f34be75654195da52f99dae23e118c4830e681
Mamather13 <casanojustine.13@gmail.com>
2026-04-30 20:05:22 +0800

    fix upload issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         URL::forceRootUrl(config('app.url'));
-         URL::forceScheme('https');
+         if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+        URL::forceRootUrl(config('app.url'));
+    }
     }
 
     /**
